b31ceec38b79a9977e378d88770aa03700392b65
Oliver <oliver01@kabsi.at>
2025-02-25 11:20:49 +0100

    tried with listboxes

--- OegegLogistics/Views/MainWindow.axaml.cs
+++ OegegLogistics/Views/MainWindow.axaml.cs
@@ -8,9 +8,5 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        
-        vehicles.ItemsSource = new string[]
-                {"cat", "camel", "cow", "chameleon", "mouse", "lion", "zebra" }
-            .OrderBy(x => x);
     }
 }
