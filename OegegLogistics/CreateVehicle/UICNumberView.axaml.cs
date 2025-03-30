cbbcd90400b6a1ebc292b8fff598d8f3b282cadf
Oliver <oliver01@kabsi.at>
2025-03-30 20:58:31 +0200

    Need to design the create vehicel flow

---
+++ OegegLogistics/CreateVehicle/UICNumberView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace OegegLogistics.CreateVehicle;
+
+public partial class UICNumberView : UserControl
+{
+    public UICNumberView()
+    {
+        InitializeComponent();
+    }
+}
