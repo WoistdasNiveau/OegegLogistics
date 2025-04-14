25e9cb19ad257611cf64d7ca870e7d8e0440a8e2
Oliver <oliver01@kabsi.at>
2025-04-14 10:47:06 +0200

    Started animation for SelectType view

--- OegegLogistics/CreateVehicle/SelectVehiceTypeView.axaml.cs
+++ OegegLogistics/CreateVehicle/SelectVehiceTypeView.axaml.cs
@@ -1,13 +1,72 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
+using Avalonia.Animation;
+using Avalonia.Animation.Easings;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Styling;
 
 namespace OegegLogistics.CreateVehicle;
 
 public partial class SelectVehiceTypeView : UserControl
 {
+    // == private fields ==
+    private double _width;
+    private double _startX;
+    private double _endX;
     public SelectVehiceTypeView()
     {
         InitializeComponent();
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+
+        TranslateTransform translateTransform = LocoSvg.RenderTransform as TranslateTransform ?? new TranslateTransform();
+        LocoSvg.RenderTransform = translateTransform;
+        
+        _width = LocoSvg.Bounds.Width;
+        _startX = -_width -2000;
+        _endX = (Bounds.Width - _width) / 2 - LocoSvg.Margin.Left;
+        
+        translateTransform.X = _startX;
+    }
+    
+
+    private async void InputElement_OnPointerEntered(object? sender, PointerEventArgs e)
+    {
+        if (LocoSvg.RenderTransform is TranslateTransform transform)
+        {
+            Animation animation = new Animation()
+            {
+                Duration = TimeSpan.FromSeconds(1),
+                Easing = new CubicEaseOut(),
+                FillMode = FillMode.Forward,
+                Children =
+                {
+                    new KeyFrame()
+                    {
+                        Cue = new Cue(0d),
+                        Setters =
+                        {
+                            new Setter(TranslateTransform.XProperty, _startX)
+                        }
+                    },
+                    new KeyFrame()
+                    {
+                        Cue = new Cue(1d),
+                        Setters =
+                        {
+                            new Setter(TranslateTransform.XProperty, _endX)
+                        }
+                    }
+                }
+            };
+            await animation.RunAsync(LocoSvg);
+        }
     }
 }
