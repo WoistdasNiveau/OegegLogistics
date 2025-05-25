0f3c6f449bd831d070636377ab01cf9971c6d52b
Oliver <oliver01@kabsi.at>
2025-05-25 12:13:41 +0200

    struggling to define NumberTExtBox

---
+++ OegegLogistics/Shared/Components/NumberTextBox.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+
+namespace OegegLogistics.Shared.Components;
+
+public class NumberTextBox : TextBox
+{
+    public NumberTextBox()
+    {
+        this.AddHandler(TextInputEvent, OnTextInput, RoutingStrategies.Tunnel);
+    }
+
+    private void OnTextInput(object? sender, TextInputEventArgs e)
+    {
+        if (!IsTextValid(e.Text))
+        {
+            e.Handled = true;
+        }
+    }
+
+    private bool IsTextValid(string input)
+    {
+        return Regex.IsMatch(input, "^[0-9]+$");
+    }
+}
---
+++ OegegLogistics/Shared/Components/NumberTextBox1.axaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+
+namespace OegegLogistics.Shared.Components;
+
+public class NumberTextBox1 : TextBox
+{
+    public NumberTextBox1()
+    {
+        this.AddHandler(TextInputEvent, OnTextInput, RoutingStrategies.Tunnel);
+    }
+
+    private void OnTextInput(object? sender, TextInputEventArgs e)
+    {
+        if (!IsTextValid(e.Text))
+        {
+            e.Handled = true;
+        }
+    }
+
+    private bool IsTextValid(string input)
+    {
+        return Regex.IsMatch(input, "^[0-9]+$");
+    }
+}
